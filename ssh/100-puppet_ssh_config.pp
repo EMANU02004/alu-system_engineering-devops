337ccfef8565b8dfc973d9346efef433b3cd5be1
EMANU02004 <e.masambu@alustudent.com>
2026-06-25 23:29:28 +0200

    initial commit

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,19 @@
-# Puppet script to create ssh config file
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+# Using puppet
+# 1. Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# 2. Your SSH client configuration must be configured to refuse to authenticate using a password
+
+include stdlib
+
+file_line { 'Refuse auth with password':
+  ensure => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+file_line { 'specify identity file path':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
